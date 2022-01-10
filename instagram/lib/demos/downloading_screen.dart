0f2405c2127c40c3df593590a2b36547c3fc68f8
SAPL <dishantvaghasiya8171@gmail.com>
2022-01-10 16:10:10 +0530

    downoading is implemneted

--- instagram/lib/demos/downloading_screen.dart
+++ instagram/lib/demos/downloading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:path_provider/path_provider.dart';
@@ -12,6 +14,13 @@ class DownloadingScreen extends StatefulWidget {
 
 class _DownloadingScreenState extends State<DownloadingScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FlutterDownloader.registerCallback(downloads);
+  }
+  static downloads (id, status, progress) { }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
@@ -25,7 +34,7 @@ class _DownloadingScreenState extends State<DownloadingScreen> {
                   showNotification: true,
                   openFileFromNotification: true,
                   url:
-                  "https://www.pinkvilla.com/imageresize/vicky_kaushal_shares_cool_pic_amid_wedding.jpg?width=752&format=webp&t=pvorg",
+                  "https://file-examples-com.github.io/uploads/2017/10/file-sample_150kB.pdf",
                   savedDir:external!.path);
               print("donesss");
             } else {
